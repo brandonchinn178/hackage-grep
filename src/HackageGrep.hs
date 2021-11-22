0a0dd831ad17464bd987bc6fcb9fd0a7e3070068
Brandon Chinn <brandonchinn178@gmail.com>
2021-11-22 13:33:26 -0800

    Implement hackageGrepConduit

--- src/HackageGrep.hs
+++ src/HackageGrep.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module HackageGrep (
   hackageGrep,
@@ -10,23 +12,28 @@ module HackageGrep (
 
   -- * Options
   HackageGrepOptions (..),
-  GrepOrderBy (..),
+  OrderPackagesBy (..),
   defaultGrepOptions,
 ) where
 
 import Conduit (ConduitT, (.|))
 import Conduit qualified
+import Control.Monad.IO.Class (liftIO)
 import Data.Text (Text)
+import Data.Text qualified as Text
+import Text.Read (readMaybe)
+import UnliftIO.Process (readProcessWithExitCode)
+import UnliftIO.Temporary (withSystemTempDirectory)
 
 {-- Options --}
 
 data HackageGrepOptions = HackageGrepOptions
-  { packageOrderBy :: GrepOrderBy
+  { packageOrderBy :: OrderPackagesBy
   , packageLimit :: Maybe Int
   }
   deriving (Show)
 
-data GrepOrderBy = MostDownloads | AToZ
+data OrderPackagesBy = MostDownloads | AToZ
   deriving (Show)
 
 defaultGrepOptions :: HackageGrepOptions
@@ -37,8 +44,11 @@ defaultGrepOptions = HackageGrepOptions
 
 {-- Grep --}
 
+type PackageName = Text
+type Pattern = Text
+
 data GrepResult = GrepResult
-  { packageName :: Text
+  { packageName :: PackageName
   , matchedLines :: [GrepResultLine] -- ^ Guaranteed to be non-empty
   }
   deriving (Show)
@@ -52,7 +62,7 @@ data GrepResultLine = GrepResultLine
 
 -- | Grep for the given pattern and return the packages the pattern is found in with the
 -- lines matching the pattern.
-hackageGrep :: HackageGrepOptions -> Text -> IO [GrepResult]
+hackageGrep :: HackageGrepOptions -> Pattern -> IO [GrepResult]
 hackageGrep opts pat =
   Conduit.runConduit $
     hackageGrepConduit opts pat .| Conduit.sinkList
@@ -60,13 +70,48 @@ hackageGrep opts pat =
 -- | Same as 'hackageGrep', except just returns the package name.
 --
 -- Faster than 'hackageGrep' as it exits after finding the first match.
-hackageGrep_ :: HackageGrepOptions -> Text -> IO [Text]
+hackageGrep_ :: HackageGrepOptions -> Pattern -> IO [PackageName]
 hackageGrep_ opts pat = map packageName <$> hackageGrep opts pat
 
 -- | Same as 'hackageGrep', except streaming the results back.
-hackageGrepConduit :: HackageGrepOptions -> Text -> ConduitT i GrepResult IO ()
-hackageGrepConduit _ _ = return ()
+hackageGrepConduit :: HackageGrepOptions -> Pattern -> ConduitT i GrepResult IO ()
+hackageGrepConduit HackageGrepOptions{..} pat = do
+  packages <- takeMaybe packageLimit <$> liftIO (getAllPackages packageOrderBy)
+  Conduit.yieldMany packages .| Conduit.concatMapMC (flip grepPackage pat)
+  where
+    takeMaybe Nothing = id
+    takeMaybe (Just x) = take x
 
 -- | Same as 'hackageGrep_', except streaming the results back.
-hackageGrepConduit_ :: HackageGrepOptions -> Text -> ConduitT i Text IO ()
+hackageGrepConduit_ :: HackageGrepOptions -> Pattern -> ConduitT i PackageName IO ()
 hackageGrepConduit_ opts pat = hackageGrepConduit opts pat .| Conduit.mapC packageName
+
+grepPackage :: PackageName -> Pattern -> IO (Maybe GrepResult)
+grepPackage package pat =
+  withSystemTempDirectory (Text.unpack $ "hackage-grep-" <> package) $ \dir -> do
+    downloadPackage dir package
+    (_, out, _) <- readProcessWithExitCode "grep" ["-rnI", Text.unpack pat, dir] ""
+    let result =
+          GrepResult
+            { packageName = package
+            , matchedLines = map parseResultLine . Text.lines . Text.pack $ out
+            }
+    return $ if null (matchedLines result) then Nothing else Just result
+  where
+    parseResultLine line =
+      case Text.splitOn ":" line of
+        filePath : lineNumberStr : rest
+          | Just lineNumber <- readMaybe $ Text.unpack lineNumberStr
+          , lineContent <- Text.intercalate ":" rest
+          -> GrepResultLine{..}
+        _ -> error $ "grep returned output in unknown format: " ++ show line
+
+{-- Hackage API --}
+
+-- TODO
+getAllPackages :: OrderPackagesBy -> IO [PackageName]
+getAllPackages _ = return []
+
+-- TODO
+downloadPackage :: FilePath -> PackageName -> IO ()
+downloadPackage _ _ = return ()
