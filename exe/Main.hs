70edb7f77f8cdc556a451d6cf1b4a5048a4b24e3
Brandon Chinn <brandonchinn178@gmail.com>
2021-11-22 12:14:37 -0800

    Add streaming version

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,15 +1,26 @@
 {-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
+import Conduit ((.|))
+import Conduit qualified
 import Data.Foldable (asum)
 import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.IO qualified as Text
 import Options.Applicative
 
 import HackageGrep
 
-parseOpts :: Parser (HackageGrepOptions, Text)
+data Options = Options
+  { grepOptions :: HackageGrepOptions
+  , grepPattern :: Text
+  , onlyPackageNames :: Bool
+  }
+
+parseOpts :: Parser Options
 parseOpts = do
   packageOrderBy <-
     option parsePackageOrderBy . mconcat $
@@ -36,13 +47,28 @@ parseOpts = do
           , showDefault
           ]
       ]
-  pat <-
+  onlyPackageNames <-
+    switch . mconcat $
+      [ long "packages-with-matches"
+      , short 'l'
+      , help "Only output the name of the package containing the pattern"
+      , showDefault
+      ]
+  grepPattern <-
     strArgument . mconcat $
       [ help "The pattern to search for"
       , metavar "PATTERN"
       ]
 
-  pure (HackageGrepOptions{..}, pat)
+  pure
+    Options
+      { grepOptions =
+          defaultGrepOptions
+            { packageOrderBy = packageOrderBy
+            , packageLimit = packageLimit
+            }
+      , ..
+      }
   where
     parsePackageOrderBy = maybeReader $ \case
       "downloads" -> Just MostDownloads
@@ -51,11 +77,21 @@ parseOpts = do
 
 main :: IO ()
 main = do
-  (opts, pat) <-
+  Options{..} <-
     execParser $
       info
         (helper <*> parseOpts)
         (fullDesc <> progDesc "Grep packages on Hackage")
-  print (opts, pat)
-  matches <- hackageGrep opts pat
-  print matches
+
+  let renderResult = if onlyPackageNames then packageName else renderGrepResult
+
+  Conduit.runConduit $
+    hackageGrepConduit grepOptions grepPattern .| Conduit.mapM_C (Text.putStrLn . renderResult)
+
+renderGrepResult :: GrepResult -> Text
+renderGrepResult GrepResult{..} =
+  Text.unlines $
+    "===== " <> packageName : map renderGrepResultLine matchedLines
+  where
+    renderGrepResultLine GrepResultLine{..} =
+      filePath <> ":" <> Text.pack (show lineNumber) <> ": " <> lineContent
--- src/HackageGrep.hs
+++ src/HackageGrep.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module HackageGrep (
   hackageGrep,
   hackageGrep_,
+  GrepResult (..),
+  GrepResultLine (..),
+  hackageGrepConduit,
+  hackageGrepConduit_,
 
   -- * Options
   HackageGrepOptions (..),
@@ -8,6 +14,8 @@ module HackageGrep (
   defaultGrepOptions,
 ) where
 
+import Conduit (ConduitT, (.|))
+import Conduit qualified
 import Data.Text (Text)
 
 {-- Options --}
@@ -29,13 +37,36 @@ defaultGrepOptions = HackageGrepOptions
 
 {-- Grep --}
 
+data GrepResult = GrepResult
+  { packageName :: Text
+  , matchedLines :: [GrepResultLine] -- ^ Guaranteed to be non-empty
+  }
+  deriving (Show)
+
+data GrepResultLine = GrepResultLine
+  { filePath :: Text
+  , lineNumber :: Int
+  , lineContent :: Text
+  }
+  deriving (Show)
+
 -- | Grep for the given pattern and return the packages the pattern is found in with the
--- lines matching the pattern. The list of matched lines is guaranteed to be non-empty.
-hackageGrep :: HackageGrepOptions -> Text -> IO [(Text, [Text])]
-hackageGrep _ _ = return []
+-- lines matching the pattern.
+hackageGrep :: HackageGrepOptions -> Text -> IO [GrepResult]
+hackageGrep opts pat =
+  Conduit.runConduit $
+    hackageGrepConduit opts pat .| Conduit.sinkList
 
 -- | Same as 'hackageGrep', except just returns the package name.
 --
 -- Faster than 'hackageGrep' as it exits after finding the first match.
 hackageGrep_ :: HackageGrepOptions -> Text -> IO [Text]
-hackageGrep_ opts pat = map fst <$> hackageGrep opts pat
+hackageGrep_ opts pat = map packageName <$> hackageGrep opts pat
+
+-- | Same as 'hackageGrep', except streaming the results back.
+hackageGrepConduit :: HackageGrepOptions -> Text -> ConduitT i GrepResult IO ()
+hackageGrepConduit _ _ = return ()
+
+-- | Same as 'hackageGrep_', except streaming the results back.
+hackageGrepConduit_ :: HackageGrepOptions -> Text -> ConduitT i Text IO ()
+hackageGrepConduit_ opts pat = hackageGrepConduit opts pat .| Conduit.mapC packageName
